009adbfefdfd91efc2fa18349a2a7061828bb717
Ahmed El-Helw <ahmedre@gmail.com>
2019-03-19 17:37:30 +0400

    Basmallah should have a shaddah in 95 and 97
    This patch replaces the basmallah glyph with the one that has a shaddah
    for suras Tin and Qadr (pages 597 and 598). Fixes #30.

---
+++ sql/03-basmallah-shaddah.sql
@@ -0,0 +1,3 @@
+-- use the basmallah glyph with a shaddah on suras teen and qadr
+-- suras 95 and 97 on pages 597 and 598. see also #30.
+update glyph_page_line set glyph_id=4 where glyph_page_line_id in(87950,88094);
